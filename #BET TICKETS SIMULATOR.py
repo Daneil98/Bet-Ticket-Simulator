8d9282bfc00f773a7cd76c7e504f7d0149e5eea2
Daniel Idahosa <75020969+Daneil98@users.noreply.github.com>
2023-11-15 13:04:01 +0100

    Latest update to #BET TICKETS SIMULATOR.py

--- #BET TICKETS SIMULATOR.py
+++ #BET TICKETS SIMULATOR.py
@@ -17,11 +17,11 @@ def combinations(n, N):
     print (f"The total number of tickets needed to cover all possible outcomes are: {total_tickets}")     
 
 
-"""def bet():
+def bet():
     #This fuction tells users about their bets and gets the kind of bet they want
     print (f"Team bets are:  {team_bet}")
     print (f"Player bets are:  {player_bet}")
-    print (f"Corner bets are:  {corner}")"""
+    print (f"Corner bets are:  {corner}")
     
     
     
@@ -39,25 +39,26 @@ def teams():
     away_str = "\n".join(away_team)
     #print(f"HOME TEAMS are: {home_str}")
     #print(f"AWAY TEAMS are: {away_str}")    
-    match = [f"{home} Vs {away}" for home, away in zip(home_team, away_team)]
-    matches_str = "\n".join(match)
-    i = 0
-    for _ in range (n**N):
-        print(matches_str)
-        if bets == "team_bet":
-            i+=1
-            print(team_bet[i])
-        elif bets == "player_bet":
-            print(player_bet[i])
-            i+=1
-        elif bets == "corner":
-            print(corner[i])
-            i+=1
-            
+    matches = [f"{home} Vs {away}" for home, away in zip(home_team, away_team)]
+    if bets == "player_bet":
+        for match in matches:
+            for bet in player_bet:
+                print(f"{match} - {bet}")        
         
+    elif bets == "team_bet":
+        for match in matches:
+            for bet in team_bet:
+                print(f"{match} - {bet}")
+                
+    elif bets == "corner":
+        for match in matches:
+            for bet in corner:
+                print(f"{match} - {bet}")
+    
         
 
 if __name__ == "__main__": 
     combinations(n, N)
+    bets()
     teams() 
     
